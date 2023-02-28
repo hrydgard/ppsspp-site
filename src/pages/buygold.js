fc53d2f70ef0cafc79238971e9dbcb18e38b0783
Henrik Rydgård <hrydgard@gmail.com>
2023-02-28 09:38:00 +0100

    Formatting fix

--- src/pages/buygold.js
+++ src/pages/buygold.js
@@ -32,76 +32,79 @@ function CategoryCard({title, description, productId}) {
 }
 
 function BuyGoldPage({goldUser}) {
-    const vcenterStyle = { display: 'flex', alignItems: 'center', width: '..', height: '..'}
 
-    var goldAlready = <>{goldUser}</>;
-    if (goldUser) {
-      goldAlready =
-        <div className="row">
-          <div className={clsx("col col--8")}>
-            <p>You already have PPSSPP Gold for Windows!</p>
-            <p>Of course, if you want to support the project a little extra, feel free!</p>
-          </div>
-        </div>;
-    }
 
-    var androidGold = <>
-      <h3>Buy PPSSPP Gold for Android</h3>
-      <p>
-        <Link className={clsx("btn btn-warning btn-large")} to="https://play.google.com/store/apps/details?id=org.ppsspp.ppssppgold">
-          <img alt="PPSSPP Gold on Google Play" src="https://developer.android.com/images/brand/en_generic_rgb_wo_60.png" />
-        </Link>
-      </p>
 
-    </>;
-    return (
-        <>
-        <br/>
-        <div className="container">
-          <div className="row">
-            <div className={clsx("col col--8")}>
-              <h1 style={vcenterStyle}><img src="/img/platform/ppsspp-icon-gold.png"  width="64px" height="64px"/>&nbsp;PPSSPP Gold</h1>
-            </div>
-          </div>
+  const vcenterStyle = { display: 'flex', alignItems: 'center', width: '..', height: '..'}
 
-          {androidGold}
-          <hr/>
-
-          <h3>Buy PPSSPP Gold for Windows</h3>
-          {goldAlready}
-
-          <p>Choose your level:</p>
-
-          <div className="row">
-            <CategoryCard title="Hangaround" description="A show of support." productId="ppsspp-gold-l1"/>
-            <CategoryCard title="Supporter" description="This will help!" productId="ppsspp-gold-l2"/>
-            <CategoryCard title="Fan" description="A strong statement." productId="ppsspp-gold-l3"/>
-          </div>
-
-          <br/>
-
-          <div className="row">
-            <CategoryCard title="Gamer" description="A real contribution." productId="ppsspp-gold-l4"/>
-            <CategoryCard title="Powergamer" description="A love of emulation." productId="ppsspp-gold-l5"/>
-            <CategoryCard title="Grandmaster" description="You totally rock!" productId="ppsspp-gold-l6"/>
-          </div>
-
-          <br/>
-
-          <div className="row">
-            <div className={clsx("col col--8")}>
-              <h3>Links and information</h3>
-              <ul>
-              <li><Link to="/docs/reference/whygold">Why buy PPSSPP Gold? What's the difference?</Link></li>
-              <li><Link to="https://play.google.com/store/apps/details?id=org.ppsspp.ppssppgold">PPSSPP Gold for Android</Link></li>
-              <li><Link to="/docs/reference/goldplatforms">PPSSPP Gold for other platforms</Link></li>
-              </ul>
-            </div>
-          </div>
+  var goldAlready = <>{goldUser}</>;
+  if (goldUser) {
+    goldAlready =
+      <div className="row">
+        <div className={clsx("col col--8")}>
+          <p>You already have PPSSPP Gold for Windows!</p>
+          <p>Of course, if you want to support the project a little extra, feel free!</p>
         </div>
+      </div>;
+  }
 
-        </>
-    )
+  var androidGold = <>
+    <h3>Buy PPSSPP Gold for Android</h3>
+    <p>
+      <Link className={clsx("btn btn-warning btn-large")} to="https://play.google.com/store/apps/details?id=org.ppsspp.ppssppgold">
+        <img alt="PPSSPP Gold on Google Play" src="https://developer.android.com/images/brand/en_generic_rgb_wo_60.png" />
+      </Link>
+    </p>
+  </>;
+
+  return (
+    <>
+    <br/>
+    <div className="container">
+      <div className="row">
+        <div className={clsx("col col--8")}>
+          <h1 style={vcenterStyle}><img src="/img/platform/ppsspp-icon-gold.png"  width="64px" height="64px"/>&nbsp;PPSSPP Gold</h1>
+        </div>
+      </div>
+
+      {androidGold}
+      <hr/>
+
+      <h3>Buy PPSSPP Gold for Windows</h3>
+      {goldAlready}
+
+      <p>Choose your level:</p>
+
+      <div className="row">
+        <CategoryCard title="Hangaround" description="A show of support." productId="ppsspp-gold-l1"/>
+        <CategoryCard title="Supporter" description="This will help!" productId="ppsspp-gold-l2"/>
+        <CategoryCard title="Fan" description="A strong statement." productId="ppsspp-gold-l3"/>
+      </div>
+
+      <br/>
+
+      <div className="row">
+        <CategoryCard title="Gamer" description="A real contribution." productId="ppsspp-gold-l4"/>
+        <CategoryCard title="Powergamer" description="A love of emulation." productId="ppsspp-gold-l5"/>
+        <CategoryCard title="Grandmaster" description="You totally rock!" productId="ppsspp-gold-l6"/>
+      </div>
+
+      <br/>
+
+      <div className="row">
+        <div className={clsx("col col--8")}>
+          <h3>Links and information</h3>
+          <ul>
+          <li><Link to="/docs/reference/whygold">Why buy PPSSPP Gold? What's the difference?</Link></li>
+          <li><Link to="https://play.google.com/store/apps/details?id=org.ppsspp.ppssppgold">PPSSPP Gold for Android</Link></li>
+          <li><Link to="/docs/reference/goldplatforms">PPSSPP Gold for other platforms</Link></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    </>
+  )
 }
 
 export default function Home() {
