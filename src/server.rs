d9fd37eedea9195b9fcf4d388b24fa5afd444791
Henrik Rydgård <hrydgard@gmail.com>
2024-01-02 18:23:44 +0100

    Add working built-in reverse proxy

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,9 @@
 use axum::{
     body::Body,
     extract::{Request, State},
-    http::uri::Uri,
+    http::{uri::Uri, HeaderValue},
     response::{IntoResponse, Response},
+    routing::get,
     Router,
 };
 use hyper::StatusCode;
@@ -22,14 +23,19 @@ pub async fn run_server(port: u16) {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    tokio::join!(serve(using_serve_dir(), port),);
+    tokio::join!(server(port),);
 }
 
-fn using_serve_dir() -> Router {
-    Router::new().nest_service("/", ServeDir::new("build"))
-}
+async fn server(port: u16) {
+    let client: Client =
+        hyper_util::client::legacy::Client::<(), ()>::builder(TokioExecutor::new())
+            .build(HttpConnector::new());
 
-async fn serve(app: Router, port: u16) {
+    let app = Router::new().nest_service("/", ServeDir::new("build"));
+    let app = app
+        .route("/api", get(reverse_proxy_handler))
+        .with_state(client);
+
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
@@ -50,11 +56,17 @@ async fn reverse_proxy_handler(
         .unwrap_or(path);
 
     // let proxy_url = "http://127.0.0.1:3000";
-    let proxy_url = "http://dev.ppsspp.org:80/";
+    // let proxy_url = "http://46.246.45.220:9101";
+    let proxy_url = "http://centraldev.ppsspp.org";
 
     let uri = format!("{}{}", proxy_url, path_query);
 
     *req.uri_mut() = Uri::try_from(uri).unwrap();
+    if let Some(host) = req.headers_mut().get_mut("host") {
+        *host = HeaderValue::from_static("centraldev.ppsspp.org:80");
+    }
+
+    println!("{:#?}", *req.headers());
 
     Ok(client
         .request(req)
