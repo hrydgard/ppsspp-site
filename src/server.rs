79eefe972b08fad235b5023b1d494a4adf0ad503
Henrik Rydgård <hrydgard@gmail.com>
2024-01-02 17:37:14 +0100

    Prepare for adding a proxy

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,17 @@
-use axum::Router;
+use axum::{
+    body::Body,
+    extract::{Request, State},
+    http::uri::Uri,
+    response::{IntoResponse, Response},
+    Router,
+};
+use hyper::StatusCode;
+use hyper_util::{client::legacy::connect::HttpConnector, rt::TokioExecutor};
 use std::net::SocketAddr;
 use tower_http::{services::ServeDir, trace::TraceLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+type Client = hyper_util::client::legacy::Client<HttpConnector, Body>;
 
 pub async fn run_server(port: u16) {
     tracing_subscriber::registry()
@@ -26,4 +36,29 @@ async fn serve(app: Router, port: u16) {
     axum::serve(listener, app.layer(TraceLayer::new_for_http()))
         .await
         .unwrap();
+}
+
+async fn reverse_proxy_handler(
+    State(client): State<Client>,
+    mut req: Request,
+) -> Result<Response, StatusCode> {
+    let path = req.uri().path();
+    let path_query = req
+        .uri()
+        .path_and_query()
+        .map(|v| v.as_str())
+        .unwrap_or(path);
+
+    // let proxy_url = "http://127.0.0.1:3000";
+    let proxy_url = "http://dev.ppsspp.org:80/";
+
+    let uri = format!("{}{}", proxy_url, path_query);
+
+    *req.uri_mut() = Uri::try_from(uri).unwrap();
+
+    Ok(client
+        .request(req)
+        .await
+        .map_err(|_| StatusCode::BAD_REQUEST)?
+        .into_response())
 }
