46e231cfc5f8567a1854cd4b5f7d8a1af43925e4
Henrik Rydgård <hrydgard@gmail.com>
2024-01-06 16:15:01 +0100

    Remove some more obsolete stuff

--- src/theme/Navbar/Content/index.js
+++
@@ -1,8 +0,0 @@
-function NavbarContentLayout({ left, right }) {
-  return (
-    <div className="navbar__inner">
-      <div className="navbar__items">{left}</div>
-      <div className="navbar__items navbar__items--right">{right}</div>
-    </div>
-  );
-}
