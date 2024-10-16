45f2b6de4a5959212bcf5d6b2a261d21f4bb504c
Henrik Rydgård <hrydgard@gmail.com>
2024-10-16 22:42:15 +0200

    Revert "Merge remote-tracking branch 'nab/icon-css-update'"
    This reverts commit 0fdf18ec46fb4d7857f1d041398de3e2a17a96d0, reversing
    changes made to a618b6de0219f5423edfceb96e19454ef6d21496.

--- static/script/main.js
+++ static/script/main.js
@@ -195,7 +195,7 @@ src="/static/img/platform/ppsspp-icon-gold.png"
 {{#else}}
 src="/static/img/platform/ppsspp-icon.png"
 {{/if}}
-class="icon-24">&nbsp;{{it.name}}
+width="22" height="22">&nbsp;{{it.name}}
 {{#else}}
 Login
 {{/if}}
@@ -212,7 +212,7 @@ src="/static/img/platform/ppsspp-icon-gold.png"
 {{#else}}
 src="/static/img/platform/ppsspp-icon.png"
 {{/if}}
-class="icon-24">&nbsp;{{it.name}}
+width="22" height="22">&nbsp;{{it.name}}
 {{#else}}
 Login
 {{/if}}
